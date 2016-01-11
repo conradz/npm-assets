648ee668d128a3fc23747e097446910ae944463b
Keith Cirkel <github@keithcirkel.co.uk>
2016-01-11 16:05:38 +0000

    style: apply eslint-config-strict

--- index.js
+++ index.js
@@ -1,132 +1,134 @@
-var fs = require('fs'),
-    path = require('path'),
-    findModules = require('find-modules'),
-    async = require('async'),
-    mkdirp = require('mkdirp');
-
+'use strict';
+var fs = require('fs');
+var path = require('path');
+var findModules = require('find-modules');
+var async = require('async');
+var mkdirp = require('mkdirp');
 module.exports = copyAssets;
 copyAssets.find = findAssets;
-
 function copyAssets(root, dest, callback) {
-    var assets;
-    findAssets(root, found);
+  var assets = null;
+  findAssets(root, found);
 
-    function found(err, a) {
-        if (err) {
-            return callback(err);
-        }
-
-        assets = a;
-        assets.forEach(function(asset) {
-            asset.dest = path.join(dest, asset.name);
-        });
-        assets = assets.filter(function(asset, i, total) {
-          for (var n = 0; n < total.length; n+=1) {
-            if(total[n].dest === asset.dest) {
-              return n === i;
-            }
-          }
-        });
-        createDirs(assets, created);
+  function found(err, foundAssets) {
+    if (err) {
+      return callback(err);
     }
 
-    function created(err) {
-        if (err) {
-            return callback(err);
+    assets = foundAssets;
+    assets.forEach(function addDestToAssetObject(asset) {
+      asset.dest = path.join(dest, asset.name);
+    });
+    assets = assets.filter(function uniqueDests(asset, i, total) {
+      for (var n = 0; n < total.length; n += 1) {
+        if (total[n].dest === asset.dest) {
+          return n === i;
         }
+      }
+    });
+    createDirs(assets, created);
+  }
 
-        copyFiles(assets, callback);
+  function created(err) {
+    if (err) {
+      return callback(err);
     }
+
+    copyFiles(assets, callback);
+  }
 }
 
 function createDirs(assets, callback) {
-    var dirs = assets
-        .map(function(a) { return path.dirname(a.dest); })
-        .filter(function (value, i, total) { return total.indexOf(value) === i; });
-
-    async.forEach(dirs, mkdirp, callback);
+  var dirs = assets
+    .map(function getDestinationDir(asset) {
+      return path.dirname(asset.dest);
+    })
+    .filter(function uniqueDests(value, i, total) {
+      return total.indexOf(value) === i;
+    });
+  async.forEach(dirs, mkdirp, callback);
 }
 
 function copyFiles(assets, callback) {
-    async.forEach(assets, copy, callback);
+  async.forEach(assets, copy, callback);
 }
 
 function copy(asset, callback) {
-    var isDone = false,
-        input = fs.createReadStream(asset.path),
-        output = fs.createWriteStream(asset.dest);
-    input.pipe(output);
-    input.on('error', done);
-    output.on('error', done);
-    output.on('finish', done);
+  var isDone = false;
+  var input = fs.createReadStream(asset.path);
+  var output = fs.createWriteStream(asset.dest);
+  input.pipe(output);
+  input.on('error', done);
+  output.on('error', done);
+  output.on('finish', done);
 
-    function done(err) {
-        if (!isDone) {
-            isDone = true;
-            callback(err);
-        }
+  function done(err) {
+    if (!isDone) {
+      isDone = true;
+      return callback(err);
     }
+  }
 }
 
 function findAssets(dir, callback) {
-    findModules(dir, foundModules);
+  findModules(dir, foundModules);
 
-    function foundModules(err, modules) {
-        if (err) {
-            return callback(err);
-        }
-
-        modules.push(dir);
-        async.map(modules, getAssets, done);
+  function foundModules(err, modules) {
+    if (err) {
+      return callback(err);
     }
 
-    function done(err, assets) {
-        if (err) {
-            return callback(err);
-        }
+    modules.push(dir);
+    async.map(modules, getAssets, done);
+  }
 
-        assets = assets.reduce(
-            function(all, a) { return all.concat(a); }, []);
-        callback(null, assets);
+  function done(err, assets) {
+    if (err) {
+      return callback(err);
     }
+
+    assets = assets.reduce(function reduceAssets(all, currentSet) {
+      return all.concat(currentSet);
+    }, []);
+    callback(null, assets);
+  }
 }
 
 function getAssets(dir, callback) {
-    var pkg = path.join(dir, 'package.json');
+  var pkg = path.join(dir, 'package.json');
+  fs.exists(pkg, checked);
 
-    fs.exists(pkg, checked);
-
-    function checked(exists) {
-        if (!exists) {
-            return callback(null, []);
-        }
-
-        fs.readFile(pkg, 'utf8', read);
+  function checked(exists) {
+    if (!exists) {
+      return callback(null, []);
     }
 
-    function read(err, contents) {
-        if (err) {
-            return callback(err);
-        }
+    fs.readFile(pkg, 'utf8', read);
+  }
 
-        try {
-            contents = JSON.parse(contents);
-        } catch (e) {
-            return callback(new Error('Error parsing "' + pkg + '"'));
-        }
-
-        var assets = contents.assets;
-        if (!Array.isArray(assets)) {
-            return callback(null, []);
-        }
-
-        assets = assets.map(function(name) {
-            return {
-                name: name,
-                path: path.join(dir, name)
-            };
-        });
-
-        callback(null, assets);
+  function read(err, contents) {
+    if (err) {
+      return callback(err);
     }
+
+    try {
+      contents = JSON.parse(contents);
+    } catch (error) {
+      return callback(new Error('Error parsing "' + pkg + '"'));
+    }
+
+    var assets = contents.assets;
+    if (!Array.isArray(assets)) {
+      return callback(null, []);
+    }
+
+    assets = assets.map(function mapAssetDescription(name) {
+      return {
+        name: name,
+        path: path.join(dir, name),
+      };
+    });
+
+    callback(null, assets);
+  }
 }
--- test.js
+++ test.js
@@ -1,61 +1,61 @@
-var assets = require('./'),
-    test = require('tap').test,
-    fs = require('fs'),
-    path = require('path'),
-    rimraf = require('rimraf');
+'use strict';
+var assets = require('./');
+var test = require('tap').test;
+var fs = require('fs');
+var path = require('path');
+var rimraf = require('rimraf');
+test('list test assets', function listTestAssets(assert) {
+  assets.find(path.join(__dirname, 'test'), found);
 
-test('list test assets', function(t) {
-    assets.find(path.join(__dirname, 'test'), found);
+  function found(err, assetList) {
+    assert.error(err, 'successful');
+    assert.equal(assetList.length, 3);
 
-    function found(err, assets) {
-        t.error(err, 'successful');
-        t.equal(assets.length, 3);
-
-        t.equal(assets[0].name, 'foo/bar.txt');
-        t.equal(
-            path.normalize(assets[0].path),
+    assert.equal(assetList[0].name, 'foo/bar.txt');
+    assert.equal(
+            path.normalize(assetList[0].path),
             path.normalize(path.join(
                 __dirname, 'test/node_modules/hello/foo/bar.txt')));
 
-        t.equal(assets[1].name, 'foo/bar.txt');
-        t.equal(
-            path.normalize(assets[1].path),
+    assert.equal(assetList[1].name, 'foo/bar.txt');
+    assert.equal(
+            path.normalize(assetList[1].path),
             path.normalize(path.join(
                 __dirname, 'test/node_modules/hello-again/foo/bar.txt')));
 
-        t.equal(assets[2].name, 'test.txt');
-        t.equal(
-            path.normalize(assets[2].path),
+    assert.equal(assetList[2].name, 'test.txt');
+    assert.equal(
+            path.normalize(assetList[2].path),
             path.normalize(path.join(
                 __dirname, 'test/test.txt')));
 
-        t.end();
-    }
+    assert.end();
+  }
 });
 
-test('copy assets', function(t) {
-    var srcDir = path.join(__dirname, 'test'),
-        destDir = path.join(__dirname, 'assets'),
-        files = ['test.txt', 'foo/bar.txt'];
-    rimraf(destDir, removed);
+test('copy assets', function copyAssets(assert) {
+  var srcDir = path.join(__dirname, 'test');
+  var destDir = path.join(__dirname, 'assets');
+  rimraf(destDir, removed);
 
-    function removed(err) {
-        t.error(err, 'removed dir');
+  function removed(err) {
+    assert.error(err, 'removed dir');
 
-        assets(srcDir, destDir, copied);
+    assets(srcDir, destDir, copied);
+  }
+
+  function copied(err) {
+    assert.error(err, 'copied assets');
+    compare('foo/bar.txt', path.join(srcDir, 'node_modules/hello'));
+    compare('test.txt', srcDir);
+
+    function compare(file, originalSrcDir) {
+      /* eslint-disable no-sync */
+      var src = fs.readFileSync(path.join(originalSrcDir, file), 'utf8');
+      var dest = fs.readFileSync(path.join(destDir, file), 'utf8');
+      assert.equal(src, dest, 'file ' + file + ' is copied');
     }
 
-    function copied(err) {
-        t.error(err, 'copied assets');
-        compare('foo/bar.txt', path.join(srcDir, 'node_modules/hello'));
-        compare('test.txt', srcDir);
-
-        function compare(file, srcDir) {
-            var src = fs.readFileSync(path.join(srcDir, file), 'utf8'),
-                dest = fs.readFileSync(path.join(destDir, file), 'utf8');
-            t.equal(src, dest, 'file ' + file + ' is copied');
-        }
-
-        t.end();
-    }
+    assert.end();
+  }
 });
